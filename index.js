609c16f729b7c9928d9cd64fe770e22354350017
Jonathan Bryant <spectrum19tech@gmail.com>
2022-01-12 14:13:17 -0800

    commit tree cb66816eace49d4f0bb312ca4e94e9a18aa45591

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(arr) {
+    const win = arr.find(({result}) => result === 'W')
+    if(win === undefined) {
+        return undefined
+    } else {
+        return win.year
+    }
+  } 
